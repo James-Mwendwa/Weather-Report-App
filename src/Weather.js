c09bb53f305b56a2c2f408da065ba2363b46d6b0
James Mwendwa <mwendwajames32@gmail.com>
2022-12-26 21:48:50 +0300

    fetch data from the  api

--- src/Weather.js
+++ src/Weather.js
@@ -1,55 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import './Weather.css';
-
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import "./Weather.css";
 
 function Weather() {
+  const [location, setLocation] = useState("");
+  const [weatherData, setWeatherData] = useState({});
 
-     const [location, setLocation] = useState("");
-     const [weatherData, setWeatherData] = useState([]);
-  
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=4cf5ac539e5d841de6360af7e94a2eaf`;
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=3cc23a56d72e010f375c8d7ba6450c78`; 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLocation("");
+  };
 
-    const searchLocation = (e) => {
-      e.preventDefault();
-       fetch(url)
-         .then((response) => response.json())
-         .then((data) => {
-           console.log(data.response);
-           setWeatherData(data);
-         })
-         .catch((error) => console.log(error));
-
-    }
-    
-    
- 
-
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setWeatherData(data);
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  }, [url]);
 
   return (
     <div className="weather">
       <div className="container">
         <div className="search">
-          <input
-            placeholder="Enter Location"
-            type="text"
-            value={location}  
-            onKeyPress={searchLocation}         
-            onChange={(e) => setLocation(e.target.value)}
-          />
+          <form onSubmit={handleSubmit}>
+            <input
+              placeholder="Enter Location"
+              type="text"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </form>
         </div>
+
         <div className="city">
-          <h2>Weather in </h2>
-          <h3>18C</h3>
+          <h2>Weather in {location}</h2>
+          {weatherData.main ? (
+            <h3>Temperature {weatherData.main.temp}</h3>
+          ) : null}
         </div>
 
         <div className="feels">
-          <h4>Humidity  </h4>
+          {weatherData.main ? (
+            <h4>Humidity {weatherData.main.humidity}</h4>
+          ) : null}
         </div>
 
         <div className="feels">
-          <h4>Wind speed </h4>
+          {weatherData.main ? (
+            <h4>Wind speed {weatherData.wind.speed} </h4>
+          ) : null}
         </div>
       </div>
     </div>
